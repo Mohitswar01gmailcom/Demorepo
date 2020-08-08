e25fff7080ddc55f038b48bce1f7761f67cb8b24
Mahesh Shivramwar <mohitswar01@gmail.com>
2020-08-09 00:45:43 +0530

    initial commit

---
+++ src/main/java/com/crm/qa/pages/HomePage.java
@@ -0,0 +1,52 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class HomePage extends TestBase {
+
+	@FindBy(xpath = "//td[contains(text(),'User: Naveen K')]")
+	@CacheLookup
+	WebElement userNameLabel;
+
+	@FindBy(xpath = "//a[contains(text(),'Contacts')]")
+	WebElement contactsLink;
+	
+	@FindBy(xpath = "//a[contains(text(),'New Contact')]")
+	WebElement newContactLink;
+	
+
+	@FindBy(xpath = "//a[contains(text(),'Deals')]")
+	WebElement dealsLink;
+
+	@FindBy(xpath = "//a[contains(text(),'Tasks')]")
+	WebElement tasksLink;
+
+	// Initializing the Page Objects:
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String verifyHomePageTitle(){
+		return driver.getTitle();
+	}
+
+	public ContactsPage clickOnContactsLink() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void clickOnNewContactLink() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean verifyCorrectUserName() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
